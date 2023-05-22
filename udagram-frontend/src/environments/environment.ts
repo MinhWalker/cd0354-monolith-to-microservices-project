698278b2a37e95c0d9efd284a7eeac5ae9925e7d
MinhWalker <minhnh421999@gmail.com>
2023-05-23 00:03:56 +0700

    add config for eks

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a17050b1db35046b8badbd76f5b1ec7d-55261279.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
